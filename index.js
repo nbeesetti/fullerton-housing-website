a4ea38f2c2a72cfccf9a83e4a70d6e175b59434e
nbeesetti <neerajabeesetti@gmail.com>
2024-05-04 11:43:42 -0700

    refined navbar with hover, make logo redirect to home

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function expandSearch() {
-    const searchInput = document.querySelector('.search-input');
-    searchInput.classList.toggle('expanded');
-    
-    if (searchInput.classList.contains('expanded')) {
-      searchInput.focus();
-    }
+  const searchInput = document.querySelector(".search-input");
+  searchInput.classList.toggle("expanded");
+
+  if (searchInput.classList.contains("expanded")) {
+    searchInput.focus();
   }
+}
+
